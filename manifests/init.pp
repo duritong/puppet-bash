6b308714c025631a663fa8168f2b7b6b042e5dd4
mh <mh@immerda.ch>
2008-01-04 09:33:25 +0000

    try new install way

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,18 +9,22 @@ class bash {
         case $operatingsystem {
                 OpenBSD: {
 
-			 package{'libiconv':
-				ensure => 'present',
-				source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/libiconv-1.9.2p3.tgz'
-			 }
-
-			 package {'gettext':
-				ensure => 'present',
-				source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/gettext-0.14.6p0.tgz',
-			 }
+#			 package{'libiconv':
+#				ensure => 'present',
+#				source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/libiconv-1.9.2p3.tgz'
+#			 }
+#
+#			 package {'gettext':
+#				ensure => 'present',
+#				source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/gettext-0.14.6p0.tgz',
+#			 }
 			 package { 'bash':
 		                ensure => 'present',
-		                source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/bash-3.2.17.tgz',
+		                #source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/bash-3.2.17.tgz',
+		                env => $opratingsystem ? {
+					openbsd => "PKG_PATH=ftp://mirror.switch.ch/pub/OpenBSD/`uname -r`/packages/`uname -m`",
+					default => undev,
+				}
         		}
                 }
         }
