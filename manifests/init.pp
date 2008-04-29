f62ee7dc67ff150d33bd7a3973e7ee462d3c4ad6
mh <mh@immerda.ch>
2008-04-29 10:02:29 +0000

    default bash stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,27 +5,44 @@
 #modules_dir { "bash": }
 
 class bash {
+    case $operatingsystem {
+        openbsd: { include bash::openbsd }
+        centos: { include bash::centos }
+        default: { include bash::base }
+    }
+}
 
-        case $operatingsystem {
-                OpenBSD: {
+class bash::base {
+    package{bash:
+        ensure => present,
+    }
+}
 
-			 package{'libiconv':
-				ensure => 'present',
-				source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/libiconv-1.9.2p3.tgz'
-			 }
 
-			 package {'gettext':
-				ensure => 'present',
-				source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/gettext-0.14.6p0.tgz',
-				require => Package[libiconv],
-			 }
-			 package { 'bash':
-		                ensure => 'present',
-		                source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/bash-3.2.17.tgz',
-				require => Package[gettext],
-        		}
-                }
-        }
+class bash::centos inherits bash::base {
+    package{'bash-completion':
+        ensure => present,
+    }
+
+}
+
+class bash::openbsd inherits bash::base {
+
+	package{'libiconv':
+	    ensure => 'present',
+		source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/libiconv-1.9.2p3.tgz'
+	}
+
+	package {'gettext':
+	    ensure => 'present',
+		source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/gettext-0.14.6p0.tgz',
+		require => Package[libiconv],
+	}
+    Package[bash]{
+	    source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/bash-3.2.17.tgz',
+        require => Package[gettext],
+    }
+    bash::deploy_profile{bash_profile_root: source => 'openbsd' }
 }
 
 define bash::deploy_profile( 
