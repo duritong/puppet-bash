288ab73b60e5d6684077a7f59b886f8c4bb5d7b1
Marcel Haerry <haerry@puzzle.ch>
2008-05-05 19:33:29 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# bash module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "bash": }
+class bash {}
