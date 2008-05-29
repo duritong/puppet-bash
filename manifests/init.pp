da24182c2254cac7519f6fb9ce8112e2985b05f7
Simon Josi <sjosi@pw-sjosi.puzzle.itc>
2008-05-29 18:39:41 +0200

    added puzzle copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,15 @@
-# modules/bash/manifests/init.pp - manage bash stuff
-# Copyright (C) 2007 admin@immerda.ch
-# GPLv3
-# adapted by Puzzle ITC - haerry+puppet(at)puzzle.ch
+#
+# bash module
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
 #
 
 #modules_dir { "bash": }
