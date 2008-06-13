57a448b09b7080c5262c6ebd5708a2f33c8723e9
mh <mh@immerda.ch>
2008-06-13 18:58:18 +0000

    merged with puzzle, added new openbsd package structure

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,15 @@
-# modules/bash/manifests/init.pp - manage bash stuff
-# Copyright (C) 2007 admin@immerda.ch
+#
+# bash module
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
 #
 
 #modules_dir { "bash": }
@@ -29,29 +39,30 @@ class bash::centos inherits bash::base {
 class bash::openbsd inherits bash::base {
 
 	package{'libiconv':
-	    ensure => 'present',
-		source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/libiconv-1.9.2p3.tgz'
+	    ensure => present,
+		source => 'ftp://mirror.switch.ch/pub/OpenBSD/${operatingsystemrelease}/packages/${hardwaremodel}/'
 	}
 
 	package {'gettext':
 	    ensure => 'present',
-		source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/gettext-0.14.6p0.tgz',
+		source => 'ftp://mirror.switch.ch/pub/OpenBSD/${operatingsystemrelease}/packages/${hardwaremodel}/'
 		require => Package[libiconv],
 	}
+
     Package[bash]{
-	    source => 'ftp://mirror.switch.ch/pub/OpenBSD/4.2/packages/i386/bash-3.2.17.tgz',
+		source => 'ftp://mirror.switch.ch/pub/OpenBSD/${operatingsystemrelease}/packages/${hardwaremodel}/'
         require => Package[gettext],
     }
     bash::deploy_profile{bash_profile_root: source => 'openbsd' }
 }
 
-define bash::deploy_profile( 
-	$source, 
-	$destination = '/root/.bash_profile',
-	$uid    = 'root',
-        $gid    = '0' ){
+define bash::deploy_profile(
+    $source,
+    $destination = '/root/.bash_profile',
+    $uid = 'root',
+    $gid = '0' ){
 
-        file {$name:
+    file {$name:
                 path => $destination,
                 owner => $uid,
                 group => $gid,
