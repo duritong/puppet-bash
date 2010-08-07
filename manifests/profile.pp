8765cb271a08ba83b2434d63ecb7a80d60470d1e
mh <mh@immerda.ch>
2010-08-07 02:12:12 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/profile.pp
+++ manifests/profile.pp
@@ -7,10 +7,10 @@ define bash::profile(
   file {$name:
     path => $destination,
     source =>   [ 
-      "puppet://$server/modules/site-bash/${fqdn}/${source}",
-      "puppet://$server/modules/site-bash/${source}",
-      "puppet://$server/modules/bash/module/${source}",
-      "puppet://$server/modules/bash/${source}"
+      "puppet:///modules/site-bash/${fqdn}/${source}",
+      "puppet:///modules/site-bash/${source}",
+      "puppet:///modules/bash/module/${source}",
+      "puppet:///modules/bash/${source}"
     ],
     owner => $uid, group => $gid, mode => 600;
   } 
--- manifests/timeout.pp
+++ manifests/timeout.pp
@@ -1,7 +1,7 @@
 class bash::timeout {
     include bash
     file{'/etc/profile.d/timeout.sh':
-        source => "puppet://$server/modules/bash/profile.d/timeout.sh",
+        source => "puppet:///modules/bash/profile.d/timeout.sh",
         require => Package['bash'],
         owner => root, group => 0, mode => 0755;
     }
